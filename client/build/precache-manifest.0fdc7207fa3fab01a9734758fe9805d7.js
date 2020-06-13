2d3500bfdd0cb27273b36c79f7048d91f5e131f4
sbebenek <36522668+sbebenek@users.noreply.github.com>
2020-06-13 18:27:07 -0400

    add build files

---
+++ client/build/precache-manifest.0fdc7207fa3fab01a9734758fe9805d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc503b7453ff08c61f6a18d61a7e7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06d628d710544d40653e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c3b1d96894230df94d39",
+    "url": "/static/css/main.a2018864.chunk.css"
+  },
+  {
+    "revision": "06d628d710544d40653e",
+    "url": "/static/js/2.6d82a887.chunk.js"
+  },
+  {
+    "revision": "ef20b9ddd7cf7cf5039dc421fe655af9",
+    "url": "/static/js/2.6d82a887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3b1d96894230df94d39",
+    "url": "/static/js/main.3dd884b1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
